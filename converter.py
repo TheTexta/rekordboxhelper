616122635218f4eac7b7c6e5201fc0a7b4119388
TheTexta <dextery777@gmail.com>
2025-07-19 15:14:28 -0300

    put my plan into this

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 ### converter.py
 # A simple GUI application to convert Spotify playlists to Rekordbox M3U format.
-# Eventually to be an all in one rekordbox soulseek helper - allowing for one to take a spotify playlist, download the tracks from soulseek, and then export to rekordbox.
+# 1. Eventually to be an all in one rekordbox soulseek helper - allowing for one to take a spotify playlist, download the tracks from soulseek, and then export to rekordbox.
+# 2. Using beets to manage songs downloaded from soulseek playlists will maintain consistent data (not as important as step 1)
 
 import pandas as pd
 import os
